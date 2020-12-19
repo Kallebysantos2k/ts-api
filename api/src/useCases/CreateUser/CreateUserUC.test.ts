d4778f982617245a0b22429f83ee9173999894df
KallebySantos <kalleby.santos2k@gmail.com>
2020-12-19 17:06:22 +0000

    feat: Test mongo connection

--- api/src/useCases/CreateUser/CreateUserUC.test.ts
+++ api/src/useCases/CreateUser/CreateUserUC.test.ts
@@ -1,4 +1,18 @@
+import * as Mongoose from 'mongoose';
 import It from 'ava';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+const Memory = new MongoMemoryServer();
+
+It.before('Starts a temporary memory cache', async () => {
+  const uri: string = await Memory.getUri();
+  await Mongoose.connect(uri);
+});
+
+It.after.always('Closing memory cache', () => {
+  Mongoose.disconnect();
+  Memory.stop();
+});
 
 It('Should create user', (t) => {
   t.pass();
